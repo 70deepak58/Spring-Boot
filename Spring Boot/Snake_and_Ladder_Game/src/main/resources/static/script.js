227cd796d7cf5229f3cd14c250e13b7aa680e876
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2023-03-31 12:30:39 +0530

    Add files via upload

--- Spring Boot/Snake_and_Ladder_Game/src/main/resources/static/script.js
+++ Spring Boot/Snake_and_Ladder_Game/src/main/resources/static/script.js
@@ -153,8 +153,17 @@ async function continuosUpdate(){
 		.then((response) => response.json())
 		.then((json) =>{console.log(json);
 		                //op_turn=json.turn;
-		                document.getElementById("player2score").innerHTML=json.score;
-	                    document.getElementById("player2turn").innerHTML=json.turn;
+		                op_score=json.score;
+		                op_turn=json.turn;
+		                p2_score=op_score;
+		                snake_bite(2,p2_score);
+                        ladder_up(2,p2_score);
+		                op_score=p2_score;
+		                
+		                
+		                
+		                document.getElementById("player2score").innerHTML=op_score;
+	                    document.getElementById("player2turn").innerHTML=op_turn;
 	                    op_turn=json.turn;
 	                    console.log(json.turn);
 		});
@@ -235,6 +244,15 @@ async function play(){
 		var j=Math.floor(Math.random() * 7);
 		document.getElementById("v").innerHTML=j;
 		my_score=my_score+j;
+		
+		//change from
+		p1_score=my_score;
+		snake_bite(1,p1_score);
+        ladder_up(1,p1_score);
+		
+		my_score=p1_score;
+		
+		//change to
 		document.getElementById("player1score").innerHTML=my_score;
 	    document.getElementById("player1turn").innerHTML=my_turn;
 	    document.getElementById("player2turn").innerHTML=op_turn;
@@ -323,6 +341,373 @@ async function playerReset(){
 		
 		
 }
+
+//my my my logic for snake
+/*
+shgdj hwdvmn
+dwe dkjwd
+e kfjw
+j deq
+'ej fw'eef ;
+fieoife
+ifmiefpq
+*/
+var img;
+var no;
+var p1_score=0;
+var p2_score=0;
+var turn=0;
+var bite=0;
+var up=0;
+var Goti_P1_Top;
+var Goti_P1_Left;
+var Goti_P2_Top;
+var Goti_P2_Left;
+
+//image change background
+var snake_no=1;
+var Stop_snake_img_chg=0;
+
+//snake function
+function snake_bite(flag,l){
+    if(flag==1){
+        //player 1 bite
+
+
+        //snake no=1
+        if(snake_no==1){
+    if(l==17){
+       p1_score-=10;
+       bite=1; 
+    }
+    if(l==54){
+        p1_score-=20;
+        bite=1; 
+     }
+     if(l==62){
+        p1_score-=43; 
+        bite=1;
+     }
+     if(l==64){
+        p1_score-=4;
+        bite=1; 
+     }
+     if(l==87){
+        p1_score-=51;
+        bite=1;
+     }
+     if(l==93){
+        p1_score-=20; 
+        bite=1;
+     }
+     if(l==95){
+        p1_score-=20;
+        bite=1;
+     }
+     if(l==98){
+        p1_score-=19; 
+        bite=1;
+     }}
+     //console.log(p1_score);
+     Goti_P1_Top=900-90*Math.floor(p1_score/10)-90;
+     Goti_P1_Left=90*Math.floor(p1_score%10)-90;
+     if(((Math.floor(p1_score/10))%2!=0)&&(p1_score/10)!=0){
+        Goti_P1_Left=900-(90*Math.floor(p1_score%10));
+        //Goti_P1_Left=0;
+        console.log("odd"+p1_score/10);
+                }
+                //bug 20 40 60
+     if((Math.floor((p1_score/10)%2==0))&&(p1_score%10==0)){
+         Goti_P1_Left=0;
+         Goti_P1_Top=900-90*Math.floor(p1_score/10);
+       // prompt("bug here");
+       console.log("bug there");
+
+    }
+    //bug2 10 20 30
+    if((Math.floor((p1_score/10)%2!=0))&&(p1_score%10==0)){
+        Goti_P1_Left=800;
+        Goti_P1_Top=900-90*Math.floor(p1_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+     document.getElementById("Goti_P1").style.top=Goti_P1_Top+"px";
+     document.getElementById("Goti_P1").style.left=Goti_P1_Left+"px";
+     console.log(Goti_P1_Top);
+     console.log(Goti_P1_Left);
+     
+if(bite==1){
+    //window.prompt("kat liya p1 ko");
+    console.log("kat liya");
+}
+     bite=0;
+    
+}
+else{
+    //player 2 bite
+
+    //snake no=1
+    if(snake_no==1){
+    if(l==17){
+        p2_score-=10; 
+        bite=1;
+     }
+     if(l==54){
+         p2_score-=20; 
+         bite=1;
+      }
+      if(l==62){
+         p2_score-=43; 
+         bite=1;
+      }
+      if(l==64){
+         p2_score-=4; 
+         bite=1;
+      }
+      if(l==87){
+         p2_score-=51; 
+         bite=1;
+      }
+      if(l==93){
+         p2_score-=20; 
+         bite=1;
+      }
+      if(l==95){
+         p2_score-=20; 
+         bite=1;
+      }
+      if(l==98){
+         p2_score-=19; 
+         bite=1;
+      }}
+
+      Goti_P2_Top=900-90*Math.floor(p2_score/10)-90;
+      Goti_P2_Left=90*Math.floor(p2_score%10)-90;
+      if(((Math.floor(p2_score/10))%2!=0)&&(p2_score/10)!=0){
+         Goti_P2_Left=900-(90*Math.floor(p2_score%10));
+         //Goti_P1_Left=0;
+         console.log("odd"+p2_score/10);
+                 }
+                            //bug 20 40 60
+     if((Math.floor((p2_score/10)%2==0))&&(p2_score%10==0)){
+        Goti_P2_Left=0;
+        Goti_P2_Top=900-90*Math.floor(p2_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+      //bug2 10 20 30
+      if((Math.floor((p2_score/10)%2!=0))&&(p2_score%10==0)){
+        Goti_P2_Left=800;
+        Goti_P2_Top=900-90*Math.floor(p2_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+      document.getElementById("Goti_P2").style.top=Goti_P2_Top+"px";
+      document.getElementById("Goti_P2").style.left=Goti_P2_Left+"px";
+      
+      document.getElementById("P2").innerHTML="P2 SCORE:"+p2_score;
+      if(bite==1){
+         // window.prompt("kat liya p2 ko");
+         console.log("kat liya p2 ko");
+      }
+      //bug here
+      if((Math.floor((p1_score/10)%2==0))&&(p1_score%10==0)){
+       // prompt("bug here");
+       console.log("bug there");
+
+    }
+      bite=0;
+      //console.log(p2_score);
+}
+
+}
+
+
+
+function ladder_up(flag2,m){
+    if(flag2==1){
+        //player 1 up
+
+        //snake no =1
+        if(snake_no==1){
+        if(m==4){
+            p1_score+=10;
+            up=1; 
+         }
+         if(m==9){
+             p1_score+=22;
+             up=1; 
+          }
+          if(m==21){
+             p1_score+=21; 
+             up=1;
+          }
+          if(m==28){
+             p1_score+=56;
+             up=1; 
+          }
+          if(m==51){
+             p1_score+=16;
+             up=1;
+          }
+          if(m==72){
+             p1_score+=19; 
+             up=1;
+          }
+          if(m==80){
+             p1_score+=19; 
+             up=1;
+          }}
+                 
+          Goti_P1_Top=900-90*Math.floor(p1_score/10)-90;
+          Goti_P1_Left=90*Math.floor(p1_score%10)-90;
+          if(((Math.floor(p1_score/10))%2!=0)&&(p1_score/10)!=0){
+Goti_P1_Left=900-(90*Math.floor(p1_score%10));
+console.log("odd"+p1_score/10);
+//Goti_P1_Left=0;
+        }
+
+         //bug here 20 40 60
+         if((Math.floor((p1_score/10)%2==0))&&(p1_score%10==0)){
+             Goti_P1_Left=0;
+             Goti_P1_Top=900-90*Math.floor(p1_score/10);
+           // prompt("bug here");
+           console.log("bug there");
+   
+        }
+
+        //bug2 10 20 30
+        //bug2 10 20 30
+    if((Math.floor((p1_score/10)%2!=0))&&(p1_score%10==0)){
+        Goti_P1_Left=800;
+        Goti_P1_Top=900-90*Math.floor(p1_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+          document.getElementById("Goti_P1").style.top=Goti_P1_Top+"px";
+          document.getElementById("Goti_P1").style.left=Goti_P1_Left+"px";
+          if(up==1){
+             // window.prompt("ladder p1");
+             console.log("laddaer P1");
+          }
+          up=0;
+    }
+    else{
+        //player 2 up
+
+        //snake no =1
+        if(snake_no==1){
+        if(m==4){
+            p2_score+=10;
+            up=1; 
+         }
+         if(m==9){
+             p2_score+=22;
+             up=1; 
+          }
+          if(m==21){
+             p2_score+=21; 
+             up=1;
+          }
+          if(m==28){
+             p2_score+=56;
+             up=1; 
+          }
+          if(m==51){
+             p2_score+=16;
+             up=1;
+          }
+          if(m==72){
+             p2_score+=19; 
+             up=1;
+          }
+          if(m==80){
+             p2_score+=19; 
+             up=1;
+          }} 
+      Goti_P2_Top=900-90*Math.floor(p2_score/10)-90;
+      Goti_P2_Left=90*Math.floor(p2_score%10)-90;
+      if(((Math.floor(p2_score/10))%2!=0)&&(p2_score/10)!=0){
+         Goti_P2_Left=900-(90*Math.floor(p2_score%10));
+         //Goti_P1_Left=0;
+         console.log("odd"+p2_score/10);
+                 }
+
+                                         //bug 20 40 60
+     if((Math.floor((p2_score/10)%2==0))&&(p2_score%10==0)){
+        Goti_P2_Left=0;
+        Goti_P2_Top=900-90*Math.floor(p2_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+       //bug2 10 20 30
+       if((Math.floor((p2_score/10)%2!=0))&&(p2_score%10==0)){
+        Goti_P2_Left=800;
+        Goti_P2_Top=900-90*Math.floor(p2_score/10);
+      // prompt("bug here");
+      console.log("bug there");
+
+   }
+      document.getElementById("Goti_P2").style.top=Goti_P2_Top+"px";
+      document.getElementById("Goti_P2").style.left=Goti_P2_Left+"px";
+          document.getElementById("P2").innerHTML="P2 SCORE:"+p2_score;
+          //bug here
+          if((Math.floor((p1_score/10)%2==0))&&(p1_score%10==0)){
+           // prompt("bug here");
+           console.log("bug there");
+   
+        }
+          if(up==1){
+             // window.prompt("ladder p2");
+             console.log("ladder p2");
+          }
+          up=0;
+    }
+
+}
+
+
+
+function win_check(){
+   if(p1_score>=100){
+      window.alert("P1 won");
+   }
+   if(p2_score>=100){
+      window.alert("P2 won");
+   }
+}
+
+
+/*
+function dice_roll(){
+	no=1+Math.floor(6*Math.random());
+    if(turn%2==0){
+        p1_score+=no;
+        snake_bite(1,p1_score);
+        ladder_up(1,p1_score);
+        win_check();
+    }
+    else{
+        p2_score+=no;
+        snake_bite(2,p2_score);
+        ladder_up(2,p2_score);
+        win_check();
+    }
+    turn++;
+}
+
+*/
+
+
+
+
+
+
 
 
 
